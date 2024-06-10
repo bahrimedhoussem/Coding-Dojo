f7637ce20e8c6647aaf1be8d841135deb20f1b93
bahrimedhoussem <houssembahri011@gmail.com>
2024-06-10 14:52:56 +0200

    hghg

---
+++ java/week1/day4/practise/Hello Human/HelloHuman-1/src/test/java/com/helloHumain/demo/HelloHumanApTest.java
@@ -0,0 +1,14 @@
+package com.helloHumain.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloHumanApTest {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
