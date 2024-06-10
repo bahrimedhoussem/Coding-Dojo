2751f1700109e52c189b2d35fd8d685822a1c53e
bahrimedhoussem <houssembahri011@gmail.com>
2024-06-10 15:47:34 +0200

    jghgh

---
+++ java/week2/Day1/core/omikuji/src/test/java/com/omikuji/AppTests.java
@@ -0,0 +1,14 @@
+package com.omikuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+
+}	
